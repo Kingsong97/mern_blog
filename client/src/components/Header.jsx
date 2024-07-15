7239764e7ccf3de3567a337c510c9d6c4f796e5e
Kingsong97 <161429740+Kingsong97@users.noreply.github.com>
2024-07-15 17:12:47 +0900

    sign-in

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -2,24 +2,36 @@ import { Link } from "react-router-dom";
 import { IoLogInOutline } from "react-icons/io5";
 import { MdDarkMode } from "react-icons/md";
 import { IoMdMenu } from "react-icons/io";
+import { useState } from "react";
 
 const Header = () => {
+    const [darkMode, setDarkMode] = useState(false);
+
+    const toggleDarkMode = () => {
+        setDarkMode(!darkMode);
+        if (darkMode) {
+            document.documentElement.classList.remove('dark');
+        } else {
+            document.documentElement.classList.add('dark');
+        }
+    };
+
     return (
-        <header id="header" className="flex justify-between items-center w-full h-16 bg-white uppercase px-40">
+        <header className="flex justify-between items-center w-full h-16 bg-white uppercase px-40 dark:bg-gray-800">
             <h1 className="logo font-caveat text-2xl">kingsong</h1>
-            <nav className="top_nav font-roboto font-light lg:flex">
+            <nav className="top_nav font-roboto font-light hidden md:flex">
                 <ul className="flex gap-4">
-                    <li><Link to="#">home</Link></li>
-                    <li><Link to="#">about</Link></li>
-                    <li><Link to="#">blog</Link></li>
-                    <li><Link to="#">portfolio</Link></li>
-                    <li><Link to="#">contact</Link></li>
+                    <li><Link to="#" className="text-black dark:text-white">home</Link></li>
+                    <li><Link to="#" className="text-black dark:text-white">about</Link></li>
+                    <li><Link to="#" className="text-black dark:text-white">blog</Link></li>
+                    <li><Link to="#" className="text-black dark:text-white">portfolio</Link></li>
+                    <li><Link to="#" className="text-black dark:text-white">contact</Link></li>
                 </ul>
             </nav>
             <div className="setting flex gap-4">
-                <div className="login text-xl"><IoLogInOutline /></div>
-                <div className="dark_mode text-xl"><MdDarkMode /></div>
-                <div className="menu_box text-xl lg:hidden"><IoMdMenu /></div>
+                <div className="login text-xl"><Link to="../pages/SignIn.jsx"><IoLogInOutline /></Link></div>
+                <div className="dark_mode text-xl cursor-pointer" onClick={toggleDarkMode}><MdDarkMode /></div>
+                <div className="menu_box text-xl md:hidden"><IoMdMenu /></div>
             </div>
         </header>
     );
--- client/src/pages/SignIn.jsx
+++ client/src/pages/SignIn.jsx
@@ -1,9 +1,103 @@
-
+import { useState } from 'react';
 
 const SignIn = () => {
-    return (
-        <div>SignIn</div>
-    )
-}
+    const [isSignUp, setIsSignUp] = useState(false);
 
-export default SignIn
+    const toggleForm = () => {
+        setIsSignUp(!isSignUp);
+    };
+
+    return (
+        <div className="min-h-screen flex items-center justify-center bg-gray-100">
+            <div className="grid grid-cols-1 md:grid-cols-2 bg-white shadow-lg rounded-lg overflow-hidden">
+                <div className="hidden md:flex justify-center items-center bg-white p-6">
+                    <img
+                        src="https://image.freepik.com/free-vector/code-typing-concept-illustration_114360-3581.jpg"
+                        alt="user login"
+                        className="w-full h-full object-cover"
+                    />
+                </div>
+                <div className="flex flex-col justify-center p-8">
+                    {isSignUp ? (
+                        <form className="space-y-6">
+                            <h1 className="text-2xl font-bold">Create Account</h1>
+                            <div className="flex items-center border-b border-gray-300 py-2">
+                                <i className='bx bx-user mr-3 text-green-500'></i>
+                                <input
+                                    type="text"
+                                    placeholder="Username"
+                                    className="appearance-none bg-transparent border-none w-full text-gray-700 py-1 px-2 leading-tight focus:outline-none"
+                                />
+                            </div>
+                            <div className="flex items-center border-b border-gray-300 py-2">
+                                <i className='bx bx-at mr-3 text-green-500'></i>
+                                <input
+                                    type="email"
+                                    placeholder="Email"
+                                    className="appearance-none bg-transparent border-none w-full text-gray-700 py-1 px-2 leading-tight focus:outline-none"
+                                />
+                            </div>
+                            <div className="flex items-center border-b border-gray-300 py-2">
+                                <i className='bx bx-lock mr-3 text-green-500'></i>
+                                <input
+                                    type="password"
+                                    placeholder="Password"
+                                    className="appearance-none bg-transparent border-none w-full text-gray-700 py-1 px-2 leading-tight focus:outline-none"
+                                />
+                            </div>
+                            <button className="bg-green-500 hover:bg-green-600 text-white font-bold py-2 px-4 rounded w-full">
+                                Sign Up
+                            </button>
+                            <div className="flex items-center justify-between">
+                                <span>Already have an account?</span>
+                                <button type="button" className="text-green-500 hover:text-green-700" onClick={toggleForm}>
+                                    Sign In
+                                </button>
+                            </div>
+                            <div className="flex justify-center space-x-4 mt-4">
+                                <a href="#" className="text-green-500"><i className='bx bxl-facebook'></i></a>
+                                <a href="#" className="text-green-500"><i className='bx bxl-twitter'></i></a>
+                                <a href="#" className="text-green-500"><i className='bx bxl-google'></i></a>
+                                <a href="#" className="text-green-500"><i className='bx bxl-github'></i></a>
+                            </div>
+                        </form>
+                    ) : (
+                        <form className="space-y-6">
+                            <h1 className="text-2xl font-bold">Sign In</h1>
+                            <div className="flex items-center border-b border-gray-300 py-2">
+                                <i className='bx bx-user mr-3 text-green-500'></i>
+                                <input
+                                    type="text"
+                                    placeholder="Username"
+                                    className="appearance-none bg-transparent border-none w-full text-gray-700 py-1 px-2 leading-tight focus:outline-none"
+                                />
+                            </div>
+                            <div className="flex items-center border-b border-gray-300 py-2">
+                                <i className='bx bx-lock mr-3 text-green-500'></i>
+                                <input
+                                    type="password"
+                                    placeholder="Password"
+                                    className="appearance-none bg-transparent border-none w-full text-gray-700 py-1 px-2 leading-tight focus:outline-none"
+                                />
+                            </div>
+                            <div className="flex items-center justify-between">
+                                <a href="#" className="text-green-500 hover:text-green-700">Forgot Password?</a>
+                            </div>
+                            <button className="bg-green-500 hover:bg-green-600 text-white font-bold py-2 px-4 rounded w-full">
+                                Sign In
+                            </button>
+                            <div className="flex items-center justify-between">
+                                <span>Dont have an account?</span>
+                                <button type="button" className="text-green-500 hover:text-green-700" onClick={toggleForm}>
+                                    Sign Up
+                                </button>
+                            </div>
+                        </form>
+                    )}
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default SignIn;
